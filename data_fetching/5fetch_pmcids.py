c90cc96f0a797b85c74891d49d27b4625824e1cf
finnmattis <finnmattis@users.noreply.github.com>
2024-07-22 13:44:06 -0700

    temp

--- data_fetching/5fetch_pmcids.py
+++ data_fetching/5fetch_pmcids.py
@@ -3,6 +3,8 @@ import xml.etree.ElementTree as ET
 import csv
 from concurrent.futures import ThreadPoolExecutor
 import logging
+import time
+import json
 
 logging.basicConfig(
     level=logging.INFO,
@@ -13,9 +15,9 @@ logging.basicConfig(
     ]
 )
 
-MAX_RETRIES = 3
-BATCH_SIZE = 200  # NCBI allows up to 200 IDs per request
-MAX_THREADS = 5  # Adjust based on your system's capabilities
+MAX_RETRIES = 10
+BATCH_SIZE = 200
+MAX_THREADS = 3  # Adjust based on your system's capabilities
 
 def fetch_pmcids_batch(batch):
     base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
@@ -35,23 +37,38 @@ def fetch_pmcids_batch(batch):
             articles = root.findall(".//PubmedArticle")
             
             for article, review in zip(articles, batch):
-                pmid = article.find(".//PMID").text
-                pmcid_elem = article.find(".//ArticleId[@IdType='pmc']")
+                article_id_list = article.find('.//ArticleIdList')
+
+                # Look for the PMCID in the ArticleIdList
+                found_pmc = False
+                if article_id_list is not None:
+                    for article_id in article_id_list.findall('ArticleId'):
+                        if article_id.get('IdType') == 'pmc':
+                            review["pmcid"] = article_id.text
+                            found_pmc = True
+                if not found_pmc:
+                    review["pmcid"] = "No PMCID found for PMID"
+                logging.info(f"{review['pmid']}:{review['pmcid']}")
                 
-                if pmcid_elem is not None:
-                    pmcid = pmcid_elem.text
-                    logging.info(f"{pmid}:{pmcid}")
+                references = article.findall('.//Reference')
+                ref_list = []
+                for reference in references:
+                    citaiton = reference.find(".//Citation")
+                    article_id_list = article.find('.//ArticleIdList')
+
+                    id_list = []
+                    if article_id_list is not None:
+                        for article_id in article_id_list.findall('ArticleId'):
+                            id_list.append((article_id.get('IdType'), article_id.text))
+                    ref_list.append((citaiton.text, id_list))
+                if len(ref_list) != 0:
+                    review["refs"] = json.dumps(ref_list)
                 else:
-                    pmcid = "Not on PMC"
-                    logging.info(f"{pmid}:{pmcid}")
-                if review["pmcid"].startswith("PMC"):
-                    review["pmcid"] = review["pmcid"][3:]
-                else:
-                    review["pmcid"] = pmcid # hacky 
-            
-            return  # Success, exit function
+                    review["refs"] = "No refs!"
+            return
         else:
             logging.warning(f"Retrying batch {pmids[:50]}... time {retry + 1} reason: {response}")
+        time.sleep(15)
     
     # If we've exhausted all retries
     for review in batch:
@@ -71,7 +88,7 @@ with open("reviews.csv", mode="r") as file:
 
 process_reviews(reviews)
 
-fieldnames = ["pmid", "title", "authors", "citation", "author", "journal", "pub_year", "pub_date", "blank1", "blank2", "doi", "license", "unpay_url", "pubmed_urls", "pmcid", "pmc_url"]
+fieldnames = ["pmid", "title", "authors", "citation", "author", "journal", "pub_year", "pub_date", "blank1", "blank2", "doi", "license", "unpay_url", "pubmed_urls", "pmcid", "refs"]
 with open("reviews_updated.csv", mode="w", newline="") as file:
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
